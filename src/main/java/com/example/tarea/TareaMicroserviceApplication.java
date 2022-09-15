b251c097c6e55f804cb0e91398957a30d5a6c3af
nayibimartin <nmartin@onemarketer.net>
2022-09-15 10:10:24 -0400

    Initial Commit

---
+++ src/main/java/com/example/tarea/TareaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tarea/TareaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
